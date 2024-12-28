b7729c6db5afce50f8468c91389e496321cab418
unknown <sandrasobel95@gmail.com>
2024-12-28 20:33:33 +0000

    setting up local storage - testing values in developer tools - succesfull

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButton ,IonCol} from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButton, IonSearchbar } from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { MyDataServiceService } from '../services/my-data-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html', 
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, RouterLink, IonButton, CommonModule, FormsModule,IonCol],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, RouterLink, IonButton, CommonModule, FormsModule,IonSearchbar],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private mds:MyDataServiceService) {}
+
+
+  ngOnInit(){
+    this.mds.set("country", "united");
+  }
 }
