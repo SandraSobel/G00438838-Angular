4104630b9340bcba077d317162bfeef7302c0777
unknown <sandrasobel95@gmail.com>
2024-12-28 19:28:24 +0000

    Created settings icon and testing button

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html', 
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, RouterLink, IonButton, CommonModule, FormsModule],
+})
+export class HomePage {
+  constructor() {}
+}
