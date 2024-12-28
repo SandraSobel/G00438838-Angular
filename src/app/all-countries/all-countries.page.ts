dbbec57a4ea41aa6ae84ab54858e33c754562f49
unknown <sandrasobel95@gmail.com>
2024-12-28 23:45:02 +0000

    Changes to implement filter per searched - testing failed - to troubleshoot

--- src/app/all-countries/all-countries.page.ts
+++ src/app/all-countries/all-countries.page.ts
@@ -16,6 +16,9 @@ import { HttpOptions } from '@capacitor/core';
 export class AllCountriesPage implements OnInit {
   countryInStorage:string = "";
   countryData:any;
+  foundCountries:any;
+  
+  
 
   options: HttpOptions = {
     url: "https://restcountries.com/v3.1/all"
@@ -26,26 +29,43 @@ export class AllCountriesPage implements OnInit {
 
   ngOnInit() { 
     this.countryData = [];  
+    this.foundCountries = [];
   }
 
   ionViewWillEnter(){
     
     this.getCountryFromStorage();
-    console.log(this.countryInStorage);
-    this.getCountriesAPI();
+    console.log(this.countryInStorage);    
+    this.searchCountryAPI();   
     
   }
 
   async getCountryFromStorage() {
     this.countryInStorage = await this.mds.get('country');
   } 
-  
-  async getCountriesAPI() {
-    let result = await this.mds.getCountriesList(this.options);
-    this.countryData = result.data;
-    console.log(this.countryData);
+   
+
+  async searchCountryAPI() {
+    await this.getAllCountriesAPI();
+    await this.getCountryFromStorage();
+     this.searchCountryByName( this.countryData, this.countryInStorage);
+    console.log(this.foundCountries);
+    console.log(this.countryInStorage);
   }
 
+  async getAllCountriesAPI() {
+    let result = await this.mds.getCountriesList(this.options);
+    this.countryData = result.data;
+   /*console.log(this.countryData);   */
+  }
+
+  searchCountryByName(countries:any[], search: string) {  
+    if(!countries || countries.length == 0){
+      console.log("empty array")           
+    }      
+    this.foundCountries = countries.filter(country=> country.name.official.toLowerCase() || country.name.nativeName.toLowerCase().includes(search.toLowerCase()));
+    
+  }
   
 
 }
